1e18c031a380baedeaa4446436129c736bea56af
speedjk1 <sack777.com@gmail.com>
2025-07-05 09:10:57 -0500

    cambio de error de anthony

--- JavaScript.js
+++ JavaScript.js
@@ -1,11 +1,7 @@
 Swal.fire({
     title : '¿Preparado para salvar el mundo? <br><br> <img src="IMG/planeta_tierra.png" width = "120px"><br>',
     html: 'IRON FIST, es un juego que mejorara tus reflejos a medida que pases de nivel, retandote cada vez mas a medida que avances y desbloqueando grandes logros al final de cada nivel, esperamos te diviertas y disfrutes de este gran juego   ',
-    icon: 'Swal.fire({icon: 'success',
-  title: 'Hecho',
-  text: 'Operación exitosa'
-
-',
+    icon: 'sucess',
     confirmButtonText: 'ESTOY PREPARADO',
     width: '50%',
     height: '80%',
